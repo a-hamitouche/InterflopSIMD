be46a729ead783940c9092deebc033d03d6afb22
a-hamitouche <ahamitouche@aneo.fr>
2023-06-15 17:56:25 +0200

    test: adding interface test for validation

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <immintrin.h>
 
+#include "simd_interface.h"
+
 int main ()
 {
   __m128 a = _mm_set1_ps (0.1f);
   __m128 b = _mm_set1_ps (0.2f);
-  __m128 c = _mm_add_ps (a, b);
+  __m128 c = _mm_set1_ps (0.0f);
+
+  interflop_simd_interface_t r_interface = init_simd_interface_t();
+
+  r_interface.sse.addf((void*) &a, (void*) &b, (void*) &c, nullptr );
 
   alignas(32) float res[4];
   _mm_store_ps (res, c);
